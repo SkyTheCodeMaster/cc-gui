0000b5ebc1a67d3f5ab75eafae178ca903eaec66
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2021-12-08 07:05:59 -0500

    commit

--- gui/frame.lua
+++ gui/frame.lua
@@ -24,19 +24,27 @@ function frame.frame(x,y,w,h,fcol,bcol)
   end
 
   -- draw the 1 pixel frame
-  local top = "\135" .. ("\131"):rep(w-2) .. "\139"
-  local bottom = "\139" .. ("\143"):rep(w-2) .. "\135"
-  local middle = "\149" .. (" "):rep(w-2) .. ""
-  local a = fcol:rep(w)
-  local b = bcol:rep(w)
+  local top = "\151" .. ("\131"):rep(w-2) .. "\148"
+  local topfg = fcol:rep(w-1) .. bcol
+  local topbg = bcol:rep(w-1) .. fcol
+
+  local mid = "\149" .. (" "):rep(w-2) .. "\149"
+  local midfg = fcol:rep(w-1) .. bcol
+  local midbg = bcol:rep(w-1) .. fcol
+  
+  local bot = "\138" .. ("\143"):rep(w-2) .. "\133"
+  local botfg = bcol:rep(w)
+  local botbg = fcol:rep(w)
+
+
   term.setCursorPos(x,y)
-  term.blit(top,b,a)
+  term.blit(top,topfg,topbg)
   term.setCursorPos(x,y+h-1)
-  term.blit(bottom,a,b)
+  term.blit(bot,botfg,botbg)
   term.setCursorPos(x,y)
   for i=1,h-1 do
     term.setCursorPos(x,y+i)
-    term.blit()
+    term.blit(mid,midfg,midbg)
   end
 end
 
