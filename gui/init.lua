0b715860c33348577809d65de485ce5e5557f2ad
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2021-12-08 20:08:35 -0500

    Safely require extensions

--- gui/init.lua
+++ gui/init.lua
@@ -6,8 +6,19 @@ gui.frame = require("gui.frame")
 -- extensions (button, progressbar)
 gui.ext = {}
 
+local function safequire(path)
+  local m = pcall(require,path)
+  if type(m) ~= "table" or next(m) == nil then
+    return nil
+  end
+  return m
+end
+
 for _,ext in pairs(fs.list("gui/ext")) do
-  gui.ext[ext] = require("gui.ext."..ext:match("(.+)%..+"))
+  local m = safequire("gui.ext."..ext:match("(.+)%..+"))
+  if m then
+    gui.ext[ext] = m
+  end
 end
 
 return gui
