132eccf9ff11ea232ea28ad54716bfb1035f90e4
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2021-12-12 09:53:45 -0500

    Add prints to process

--- process.py
+++ process.py
@@ -4,12 +4,15 @@ import re
 
 with open("downloaded") as f:
   downloads = json.loads(f.read())
+  print(downloads)
 
 def canRemove(line:str):
   return re.match("-- @module\[kind=.*?\] .*",line)
 
 for file in downloads:
+  print("opening",file)
   with open(file) as r:
     lines = [line for line in r if not canRemove(line)]
+    print("writing",file)
     with open(file,"w") as w:
       w.write("\n".join(lines))
